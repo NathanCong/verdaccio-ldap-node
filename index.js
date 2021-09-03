13fb7182ff78b4f2eb72786a2f3947c1ac000ddb
NathanCong <congyunan@xdf.cn>
2021-09-03 19:08:19 +0800

    修复Bug

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 /**
  * verdaccio-ldap-node
  */
- 'use strict';
+"use strict";
 
- const LdapClient = require('node-ldap');
- 
- function Auth(config, stuff) {
-   const self = Object.create(Auth.prototype);
-   self._users = {};
-   // config for this module
-   self._config = config;
-   // verdaccio logger
-   self._logger = stuff.logger;
-   return self;
- }
- 
- Auth.prototype.authenticate = function(username, password, callback) {
-   // initial LDAP client
-   const client = new LdapClient(this._config);
-   // start auth
-   client.auth(`${username}@xdf.cn`, password)
-     .then(function() {
-       callback(null, [ username ]);
-     })
-     .catch(function(error) {
-       this._logger.warn({ username, error }, `verdaccio-ldap error ${error}`);
-       callback(null, false);
-     })
-     .finally(function() {
-       client.disconnect();
-     });
- };
- 
- module.exports = Auth;
- 
+const LdapClient = require("node-ldap");
+
+function Auth(config, stuff) {
+  const _this = Object.create(Auth.prototype);
+  _this._users = {};
+  // config for this module
+  _this._config = config;
+  // verdaccio logger
+  _this._logger = stuff.logger;
+  return _this;
+}
+
+Auth.prototype.authenticate = function (username, password, callback) {
+  const _this = this;
+  // initial LDAP client
+  const client = new LdapClient(_this._config);
+  // start auth
+  client
+    .auth(`${username}@xdf.cn`, password)
+    .then(function () {
+      callback(null, [username]);
+    })
+    .catch(function (error) {
+      _this._logger.warn({ username, error }, `verdaccio-ldap error ${error}`);
+      callback(null, false);
+    })
+    .finally(function () {
+      client.disconnect();
+    });
+};
+
+module.exports = Auth;
