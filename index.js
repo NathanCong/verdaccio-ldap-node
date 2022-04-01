18398ac2c0047ce76ab7c8fcf0890c0ad7a3a2ca
congyunan <congyunan@xdf.cn>
2022-04-01 16:53:37 +0800

    优化LDAP连接流程

--- index.js
+++ index.js
@@ -15,22 +15,30 @@ function Auth(config, stuff) {
 
 Auth.prototype.authenticate = function(username, password, callback) {
   const _this = this;
-  // 1.Initial LDAP client
+  // 1.Create LDAP client
   const client = ldapjs.createClient({
     url: (_this._config || {}).ldapUrl,
     timeout: 2000,
     connectTimeout: 2000,
     reconnect: false,
   });
-  // 2.Check username & password
-  client.bind(`${username}@xdf.cn`, password, function(err) {
-    if (err) {
-      _this._logger.warn({ username, err }, `verdaccio-ldap-node error ${err}`);
-      callback(null, false);
-      return;
-    }
-    client.unbind();
-    callback(null, [username]);
+  // 2.Check LDAP connection
+  client.on('error', function(err) {
+    _this._logger.warn({ username, err }, `verdaccio-ldap-node error ${err}`);
+    callback(null, false);
+  });
+  client.on('connect', function() {
+    // 3.Check username & password
+    client.bind(`${username}@xdf.cn`, password, function(err) {
+      if (err) {
+        _this._logger.warn({ username, err }, `verdaccio-ldap-node error ${err}`);
+        callback(null, false);
+      } else {
+        client.unbind();
+        callback(null, [username]);
+      }
+      client.destroy();
+    });
   });
 };
 
